642f2ca574e62f5b0249fa57b554b95820147813
Niccolo Benetti <niccolo.benetti@mail.polimi.it>
2024-03-06 19:22:04 +0100

    added input function used for the parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <cstdlib>
 #include <iostream>
+#include <cstdio> 
 
-#include "main.hpp"
+// input(const char*, char**) : reads the file specified by filename and saves its content into fileContent
+void input(const char *filename, char **fileContent) {
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        std::cout << "Can't open the file " << filename << std::endl;
+        return;
+    }
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
-  std::cout << "Hello world" << std::endl;
-  return EXIT_SUCCESS;
+    // Gets the size of the file:
+    fseek(file, 0, SEEK_END); 
+    long fileSize = ftell(file); 
+    fseek(file, 0, SEEK_SET); 
+
+    *fileContent = (char *)malloc(fileSize + 1); 
+    if (*fileContent == NULL) {
+        std::cout << "Malloc error" << std::endl;
+        fclose(file);
+        return;
+    }
+
+    fread(*fileContent, 1, fileSize, file); 
+    (*fileContent)[fileSize] = '\0'; 
+
+    fclose(file); 
+}
+
+int main(/*[[maybe_unused]] int argc, [[maybe_unused]] char *argv[]*/) {
+    const char *filename = "path_to_file.txt"; // Path to the file you want to read
+    char *fileContent = NULL;
+
+    input(filename, &fileContent);
+
+    return EXIT_SUCCESS;
 }
