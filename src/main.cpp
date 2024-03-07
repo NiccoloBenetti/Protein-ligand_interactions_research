0f677f4e16e7496a9b46c133d37574f3171f07fb
Niccolo Benetti <niccolo.benetti@mail.polimi.it>
2024-03-07 11:15:02 +0100

    Added file input from command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <cstdio> 
 
 // input(const char*, char**) : reads the file specified by filename and saves its content into fileContent
-void input(const char *filename, char **fileContent) {
+void input(char *filename, char **fileContent) {
     FILE *file = fopen(filename, "rb");
     if (!file) {
         std::cout << "Can't open the file " << filename << std::endl;
@@ -28,11 +28,20 @@ void input(const char *filename, char **fileContent) {
     fclose(file); 
 }
 
-int main(/*[[maybe_unused]] int argc, [[maybe_unused]] char *argv[]*/) {
-    const char *filename = "path_to_file.txt"; // Path to the file you want to read
+int main(int argc, char *argv[]) {  // First argument: PDB file, then a non fixed number of Mol2 files
+    //const char *filename = "path_to_file.txt"; // Path to the file you want to read
     char *fileContent = NULL;
 
-    input(filename, &fileContent);
+    // Prints the files passed from line (argc, argv)
+    if(argc >= 2){
+        printf("Ci sono %d file passati:\n", argc - 1);
+        std::cout << "1-" << "Protein: " << argv[1] << std::endl;
+        for(int i = 2; i < argc; i++) {
+            std::cout << i << "-Ligand: " << argv[i] << std::endl;
+        }
+    }
+
+    //input(argv[1], &fileContent);
 
     return EXIT_SUCCESS;
 }
