6e9f635ea930833e5def22baebd15519a5a1c4ec
Niccolo Benetti <160759878+NiccoloBenetti@users.noreply.github.com>
2025-04-27 10:59:49 +0200

    Added support folders

---
+++ support/deleteCSV.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Cambia directory nella directory Testing, che si trova nella directory corrente
+cd Testing
+
+# Loop attraverso tutte le sottodirectory
+for dir in */; do
+    # Costruisce il percorso completo del file interaction.csv in ogni sottodirectory
+    interaction_csv="${dir}interactions.csv"
+    
+    # Controlla se il file interaction.csv esiste
+    if [[ -f "$interaction_csv" ]]; then
+        # Elimina il file interaction.csv
+        rm "$interaction_csv"
+        echo "Removed $interaction_csv"
+    else
+        echo "No interactions.csv file in $dir"
+    fi
+done
+
